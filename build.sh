047052609b00fe1626cd4a0931791fdeefdad770
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2010-07-22 13:09:48 +0100

    Small improvement for build script.

--- build.sh
+++ build.sh
@@ -6,14 +6,14 @@ python setup.py build &&
 python setup.py install --user &&
 pushd docs &&
 pushd _build/html &&
-(ls -1 | xargs rm -r) &&
+([ `ls -1` ] && (ls -1 | xargs rm -r)) ||
 popd &&
-make doctest &&
 make html &&
 make latex &&
 pushd _build/latex &&
 make all-pdf &&
 popd &&
+make doctest &&
 popd  &&
 python setup.py sdist --formats=zip
 ) || popd
