8a1f9065f6ec811004d2b9dc25dd47946caecf03
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2010-08-10 21:16:59 +0100

    Implemented Erik's algorithm for checking n-monotonicity (+ extreme n-monotone lower probabilities as a bonus, although this doesn't work very well with cddlib).

--- docs/lowprev/lowprob.rst
+++ docs/lowprev/lowprob.rst
@@ -1,5 +1,6 @@
 .. testsetup::
 
+   from improb import PSpace, Gamble, Event
    from improb.lowprev.lowprob import LowProb
 
 .. module:: improb.lowprev.lowprob
--- improb/lowprev/lowprob.py
+++ improb/lowprev/lowprob.py
@@ -17,7 +17,10 @@
 
 """Coherent lower probabilities."""
 
+import cdd
+import collections
 from fractions import Fraction
+import itertools
 import random
 
 from improb import PSpace, Gamble, Event
@@ -65,6 +68,9 @@ class LowProb(LowPoly):
       1 2 : 3/20
     0 1 2 : 7/10
     """
+
+    _constraints_n_monotone = {}
+    """Caches the constraints calculated for n-monotonicity."""
 
     def _clear_cache(self):
         LowPoly._clear_cache(self)
@@ -295,3 +301,232 @@ class LowProb(LowPoly):
             if self.number_cmp(value) < 0:
                 return False
         return True
+
+    def is_n_monotone(self, monotonicity=None):
+        """Has the lower probability the given level of monotonicity?
+
+        .. warning::
+
+           The lower probability must be defined for all events. If
+           needed, call :meth:`~improb.lowprev.lowpoly.LowPoly.extend`
+           first.
+        """
+        # iterate over all constraints
+        for constraint in self.get_constraints_n_monotone(
+            self.pspace, xrange(1, monotonicity + 1)):
+            # check the constraint
+            if self.number_cmp(
+                sum(coeff * self[event, True][0]
+                    for coeff, event in itertools.izip(
+                        constraint, self.pspace.subsets()))) < 0:
+                return False
+        return True
+
+    @classmethod
+    def get_constraints_n_monotone(cls, pspace, monotonicity=None):
+        """Yields constraints for lower probabilities with given
+        monotonicity. This follows the algorithm described in: *Erik
+        Quaeghebeur and Gert de Cooman. Extreme lower probabilities.
+        Fuzzy Sets and Systems 159(16), pages 2163-2175*.
+
+        The elements are the coefficients on the lower probabilities
+        of the events, following the ordering of ``pspace.subsets()``.
+        The constant term is always zero so is omitted.
+
+        .. note::
+
+            To get *all* the constraints for n-monotonicity, call this
+            method with monotonicity=xrange(1, n + 1).
+
+        .. note::
+
+            The result is cached, for speed.
+
+        >>> for mono in xrange(1, 3 + 1):
+        ...     print("{0} monotonicity:".format(mono))
+        ...     print(" ".join("{0:<3}".format("".join(i for i in event))
+        ...                    for event in PSpace("abc").subsets()))
+        ...     for constraint in sorted(
+        ...         LowProb.get_constraints_n_monotone("abc", mono)):
+        ...         print(" ".join("{0:<3}".format(value)
+        ...                        for value in constraint))
+        1 monotonicity:
+            a   b   c   ab  ac  bc  abc
+        -1  0   0   1   0   0   0   0  
+        -1  0   1   0   0   0   0   0  
+        -1  1   0   0   0   0   0   0  
+        0   -1  0   0   0   1   0   0  
+        0   -1  0   0   1   0   0   0  
+        0   0   -1  0   0   0   1   0  
+        0   0   -1  0   1   0   0   0  
+        0   0   0   -1  0   0   1   0  
+        0   0   0   -1  0   1   0   0  
+        0   0   0   0   -1  0   0   1  
+        0   0   0   0   0   -1  0   1  
+        0   0   0   0   0   0   -1  1  
+        2 monotonicity:
+            a   b   c   ab  ac  bc  abc
+        0   0   0   1   0   -1  -1  1  
+        0   0   1   0   -1  0   -1  1  
+        0   1   0   0   -1  -1  0   1  
+        1   -1  -1  0   1   0   0   0  
+        1   -1  0   -1  0   1   0   0  
+        1   -1  0   0   0   0   -1  1  
+        1   0   -1  -1  0   0   1   0  
+        1   0   -1  0   0   -1  0   1  
+        1   0   0   -1  -1  0   0   1  
+        3 monotonicity:
+            a   b   c   ab  ac  bc  abc
+        -1  1   1   1   -1  -1  -1  1  
+        2   -1  -1  -1  0   0   0   1  
+        """
+        pspace = PSpace.make(pspace)
+        # check type
+        if monotonicity is None:
+            raise ValueError("specify monotonicity")
+        elif isinstance(monotonicity, collections.Iterable):
+            # special case: return it for all values in the iterable
+            for mono in monotonicity:
+                for constraint in cls.get_constraints_n_monotone(pspace, mono):
+                    yield constraint
+            return
+        elif not isinstance(monotonicity, (int, long)):
+            raise TypeError("monotonicity must be integer")
+        # check value
+        if monotonicity <= 0:
+            raise ValueError("specify a strictly positive monotonicity")
+        # try cached version
+        try:
+            constraints = cls._constraints_n_monotone[len(pspace), monotonicity]
+        except KeyError:
+            pass
+        else:
+            for constraint in constraints:
+                yield constraint
+            return
+        # not in cache: calculate it
+        constraints = []
+        powerset = list(pspace.subsets())
+        event_index = dict((event, index)
+                           for index, event in enumerate(powerset))
+        if monotonicity == 1:
+            # for every generating event
+            for event in pspace.subsets(empty=False):
+                # k=1 monotonicity constraint
+                # note: we can restrict to events of just one size less
+                # the others will follow by transitivity
+                for subset in pspace.subsets(event,
+                                             full=False, size=len(event) - 1):
+                    constraint = [0] * len(powerset)
+                    constraint[event_index[event]] = 1
+                    constraint[event_index[subset]] = -1
+                    constraints.append(tuple(constraint))
+        else: # monotonicity >= 2
+            # alias, for convenience
+            k = monotonicity
+            # get constraints for lower levels (to check redundancy)
+            other_constraints = set(
+                cls.get_constraints_n_monotone(pspace, xrange(1, k)))
+            # for every generating event
+            for event in pspace.subsets(empty=False):
+                # iterate over all combinations of k subsets (non-empty,
+                # and not the event itself)
+                for k_subsets in itertools.combinations(
+                    pspace.subsets(event, empty=False, full=False), k):
+                    # calculate union and intersection
+                    union = reduce(lambda x, y: x | y, k_subsets)
+                    intersection = reduce(lambda x, y: x & y, k_subsets)
+                    if union != event:
+                        # not admissible
+                        continue
+                    if intersection in k_subsets:
+                        # leads to redundant constraint
+                        continue
+                    # initialize constraint
+                    constraint = [0] * len(powerset)
+                    constraint[event_index[event]] = 1
+                    # iterate over all subsets of k_subsets
+                    intersections = set()
+                    for j in xrange(1, len(k_subsets) + 1):
+                        for j_subsets in itertools.combinations(k_subsets, j):
+                            intersection = reduce(lambda x, y: x & y, j_subsets)
+                            constraint[event_index[intersection]] += (-1) ** j
+                    constraint = tuple(constraint) # tuple is hashable
+                    # XXX extra check for redundancy
+                    if constraint in other_constraints:
+                        continue
+                    # ok, append it
+                    other_constraints.add(constraint)
+                    constraints.append(constraint)
+        # cache the result
+        cls._constraints_n_monotone[len(pspace), monotonicity] = constraints
+        # return an iterator to the constraints
+        for constraint in constraints:
+            yield constraint
+
+    @classmethod
+    def make_extreme_n_monotone(cls, pspace, monotonicity=None):
+        """Yield extreme lower probabilities with given monotonicity.
+
+        .. warning::
+
+           Currently this doesn't work very well except for the cases
+           below.
+
+        >>> lprs = list(LowProb.make_extreme_n_monotone('abc', monotonicity=2))
+        >>> len(lprs)
+        8
+        >>> all(lpr.is_coherent() for lpr in lprs)
+        True
+        >>> all(lpr.is_n_monotone(2) for lpr in lprs)
+        True
+        >>> all(lpr.is_n_monotone(3) for lpr in lprs)
+        False
+        >>> lprs = list(LowProb.make_extreme_n_monotone('abc', monotonicity=3))
+        >>> len(lprs)
+        7
+        >>> all(lpr.is_coherent() for lpr in lprs)
+        True
+        >>> all(lpr.is_n_monotone(2) for lpr in lprs)
+        True
+        >>> all(lpr.is_n_monotone(3) for lpr in lprs)
+        True
+        >>> lprs = list(LowProb.make_extreme_n_monotone('abcd', monotonicity=2))
+        >>> len(lprs)
+        41
+        >>> all(lpr.is_coherent() for lpr in lprs)
+        True
+        >>> all(lpr.is_n_monotone(2) for lpr in lprs)
+        True
+        >>> all(lpr.is_n_monotone(3) for lpr in lprs)
+        False
+        >>> # cddlib hangs on these...
+        >>> #lprs = list(LowProb.make_extreme_n_monotone('abcde', monotonicity=2))
+        >>> #lprs = list(LowProb.make_extreme_n_monotone('abcd', monotonicity=3))
+        >>> #lprs = list(LowProb.make_extreme_n_monotone('abcd', monotonicity=4))
+        """
+        pspace = PSpace.make(pspace)
+        # constraint for empty set and full set
+        matrix = cdd.Matrix(
+            [[0] + [1 if event.is_false() else 0 for event in pspace.subsets()],
+             [-1] + [1 if event.is_true() else 0 for event in pspace.subsets()]],
+            linear=True,
+            number_type='fraction')
+        # constraints for monotonicity
+        matrix.extend([(0,) + constraint
+                       for constraint
+                       in cls.get_constraints_n_monotone(
+                           pspace, xrange(1, monotonicity + 1))
+                       ])
+        # calculate extreme points
+        matrix.rep_type = cdd.RepType.INEQUALITY
+        poly = cdd.Polyhedron(matrix)
+        # convert these points back to lower probabilities
+        #print(matrix) # debug
+        #print(poly.get_generators()) # debug
+        for vert in poly.get_generators():
+            yield cls(
+                pspace=pspace,
+                lprob=dict((event, vert[1 + index])
+                           for index, event in enumerate(pspace.subsets())),
+                number_type='fraction')
