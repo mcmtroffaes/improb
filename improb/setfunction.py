faa78c813142bfef4ac0b272cedc18239043bf58
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2011-02-09 16:38:49 +0000

    Minor optimizations: using iterators rather than lists in a few places in get_choquet.

--- improb/setfunction.py
+++ improb/setfunction.py
@@ -251,11 +251,12 @@ class SetFunction(collections.MutableMapping, cdd.NumberTypeable):
            KeyError: Event(pspace=PSpace(['a', 'b', 'c']), elements=set(['c']))
         """
         gamble = self.pspace.make_gamble(gamble)
-        values = sorted(set(gamble.values()))  # set to get unique values
+        values = sorted(set(gamble.itervalues()))  # set to get unique values
         coeffs = (current - previous  # v0, v1-v0, ...
                   for current, previous
                   in itertools.izip(values, [0] + values))
-        level = lambda t: (key for key, value in gamble.items() if value >= t)
+        level = lambda t: (key for key, value in gamble.iteritems()
+                           if value >= t)
         events = (Event(gamble.pspace, level(value)) for value in values)
         return sum(coeff * self[event]
                    for coeff, event in itertools.izip(coeffs, events))
