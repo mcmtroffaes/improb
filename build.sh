5e6739fa5079f59aa43ac763321c282747e12c61
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2012-07-02 15:22:23 +0100

    Updated build script for removed submodule.

--- build.sh
+++ build.sh
@@ -5,9 +5,6 @@ pushd .
 python setup.py build &&
 python setup.py install --user &&
 pushd docs &&
-pushd _build/html &&
-(([ "`ls -1 | xargs`" ] && (ls -1 | xargs rm -r)) || true) &&
-popd &&
 make html &&
 make latex &&
 pushd _build/latex &&
